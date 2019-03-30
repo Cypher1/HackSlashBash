ea0a83d06d0038050286c820dfdf479d40f5f2b7
Joshua Pratt <jp10010101010000@gmail.com>
2019-03-30 11:36:53 +0900

    Look

--- actions/look.sh
+++ actions/look.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+choose "What at" "${loc}/items" "inventory"
 
+if [ -z "${choice}" ]
+then
+  echo "You can't seem to find the ${fail}."
+  return
+fi
+
+echo "You see $(get "items/${choice}/description")"
